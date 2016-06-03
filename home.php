06dee977577270580c71760fcda8cdcb2240d94e
Chris Mott <christopheramott@sky.com>
2016-06-03 21:28:56 +0100

    Updated database stuff

--- home.php
+++ home.php
@@ -11,20 +11,29 @@
 <body>
 
     <div id="wrapper">
-        <div class="nav">
-            <h1><img src="images/favicon2.png">MiSlice Wall</h1>
+        <div id="nav">
+            <div id="navLeft">
+                <a href="admin/logout.php"><div id="btnLogout">Logout</div></a>
+            </div>
+            <div id="navCenter">
+                <h1 id="navLogo">MiSlice Wall</h1>
+            </div>
+            <div id="navRight">
+                <a href="admin/index.php"><div id="btnAccount">Account</div></a>
+            </div>
         </div>
         <div class="posts">
             <?php
                 try {
 
-                    $stmt = $db->query('SELECT postID, postDesc, postDate FROM blog_posts ORDER BY postID DESC');
+                    $stmt = $db->query('SELECT blog_members.username, postID, postDesc, postDate FROM blog_members INNER JOIN blog_posts ON blog_members.memberID = blog_posts.memberID ORDER BY postID DESC');
                     while($row = $stmt->fetch()){
                     
-                        echo '<div>';
+                        echo '<div class="thepost">';
+                            echo '<p>'.$row['username'].'</p>';
                             echo '<p>'.date('jS M Y H:i:s', strtotime($row['postDate'])).'</p>';
                             echo '<p>'.$row['postDesc'].'</p>';                
-                            echo '<p><a href="viewpost.php?id='.$row['postID'].'">Expand</a></p>';                
+                            echo '<p><a href="viewpost.php?id='.$row['postID'].'"><div id="expand">Expand</div></a></p>';                
                         echo '</div>';
 
                     }
