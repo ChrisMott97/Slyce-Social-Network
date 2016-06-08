f04878aa2b9be1c6b9cc5ce2f80fff48e48feb8c
Chris Mott <christopheramott@sky.com>
2016-06-08 21:24:20 +0100

    Added profile page

--- user/profile.php
+++ user/profile.php
@@ -1,11 +1,30 @@
-<?php require('includes/config.php'); ?>
+<?php
+//include config
+require_once('../includes/config.php');
+
+//if not logged in redirect to login page
+if(!$user->is_logged_in()){ header('Location: login.php'); }
+
+/*try {
+
+    $stmt = $db->prepare('SELECT firstName FROM members WHERE memberID=:theid');
+    $stmt->execute(array('theid' => $user->get_user_id()));
+    $row = $stmt->fetch();
+                    
+    echo '<h1>Hello, '.$row["firstName"].'</h1>';
+
+    } catch(PDOException $e) {
+        echo $e->getMessage();
+    } */
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <title>Public Wall</title>
     <link rel="stylesheet" href="style/normalize.css">
-    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="/style/main.css">
     <link href='http://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
 </head>
 <body>
@@ -13,20 +32,20 @@
     <div id="wrapper">
         <div id="nav">
             <div id="navLeft">
-                <a href="admin/logout.php"><div id="btnLogout">Logout</div></a>
+                <a href="/admin/logout.php"><div id="btnLogout">Logout</div></a>
             </div>
             <div id="navCenter">
-                <h1 id="navLogo">MiSlice Wall</h1>
+                <h1 id="navLogo">Profile</h1>
             </div>
             <div id="navRight">
-                <a href="/user/profile.php"><div id="btnProfile">Profile</div></a>
+                <a href="/home.php"><div id="btnNewsFeed">News Feed</div></a>
             </div>
         </div>
         <div class="posts">
             <?php
                 try {
 
-                    $stmt = $db->query('SELECT members.username, postID, postDesc, postDate FROM members INNER JOIN posts ON members.memberID = posts.memberID ORDER BY postID DESC');
+                    $stmt = $db->query('SELECT members.username, postID, postDesc, postDate FROM members INNER JOIN posts ON members.memberID = posts.memberID WHERE members.memberID='.$user->get_user_id());
                     while($row = $stmt->fetch()){
                     
                         echo '<div class="thepost">';
