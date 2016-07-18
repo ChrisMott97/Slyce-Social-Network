3af8c938efe290456ecad70bbbfdd016b2bfa092
Chris Mott <christopheramott@sky.com>
2016-07-18 07:46:44 +0100

    Updated expand button ;D

--- home.php
+++ home.php
@@ -5,36 +5,54 @@
     <meta charset="utf-8">
     <title>Slyce Wall</title>
     <link rel="stylesheet" href="style/main.css">
-    <link rel="stylesheet" href="style/materialize.css">
+    <link type="text/css" rel="stylesheet" href="style/materialize.css"  media="screen,projection"/>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
-    <script src="materialize-src/js/bin/materialize.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    
 </head>
 <body>
-
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="materialize-src/js/bin/materialize.js"></script>
+    <script>
+    $(document).ready(function() {
+  $('.modal-trigger').leanModal();
+});
+    </script>
     <div id="wrapper">
         <?php include('includes/navigation.php');?>
         <div class="posts">
            <?php include('includes/createPostForm.php');?>
+           
             <?php
                 try {
 
-                    $stmt = $db->query('SELECT members.username, postID, postDesc, postDate, canExpand FROM members INNER JOIN posts ON members.memberID = posts.memberID ORDER BY postID DESC');
+                    $stmt = $db->query('SELECT members.username, postID, postDesc, postDate, canExpand, postCont FROM members INNER JOIN posts ON members.memberID = posts.memberID ORDER BY postID DESC');
                     while($row = $stmt->fetch()){
                         echo '<div class="row">';
                             echo '<div class="col s12 m12">';
                                 echo '<div class="card">';
                                     echo '<div class="card-content black-text">';
-                                        echo '<span class="card-title">'.$row['username'].'</span>';
+                                        echo '<span class="card-title ">'.$row['username'].'</span>';
                                         echo '<p>'.date('jS M Y H:i:s', strtotime($row['postDate'])).'</p>';
                                         echo '<p>'.$row['postDesc'].'</p>';
                                     echo '</div>';
                                     echo '<div class="card-action">';
                                         echo '<a href="#">Like</a>';
                                         if ($row['canExpand'] == 1) {
-                                            echo '<a href="viewpost.php?id='.$row['postID'].'">Expand</a>';
+                                            //echo '<a href="viewpost.php?id='.$row['postID'].'">Expand</a>';
+                                            echo '<a href="#modal'.$row['postID'].'" class="modal-trigger">Expand</a>';
                                         } else {
                                         }
+                                    echo '</div>';
+                                    echo '<div id="modal'.$row['postID'].'" class="modal">';
+                                        echo '<div class="modal-content">';
+                                            echo '<h4>'.$row['username'].'</h4>';
+                                            echo '<p>'.$row['postCont'].'</p>';
+                                        echo '</div>';
+                                        echo '<div class="modal-footer">';
+                                            echo '<a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Return</a>';
+                                        echo '</div>';
                                     echo '</div>';
                                 echo '</div>';
                             echo '</div>';
--- includes/createPostForm.php
+++ includes/createPostForm.php
@@ -62,7 +62,16 @@
             }
         }
         ?>
-               
+          <div class="row">
+            <form class="col s12">
+              <div class="row">
+                <div class="input-field col s12">
+                  <textarea id="textarea1" class="materialize-textarea"></textarea>
+                  <label for="textarea1">Textarea</label>
+                </div>
+              </div>
+            </form>
+          </div>               
         <form action='' method='post'>
                    
             <textarea name='postCont' cols='100' rows='10' ><?php if(isset($error)){ echo $_POST['postCont'];}?></textarea></p>
