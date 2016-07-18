37e4789060c233e7a3da3305c478ed017c5147b3
Chris <christopheramott@sky.com>
2016-07-18 21:58:36 +0100

    Updated mobile compatitiblity

--- profile.php
+++ profile.php
@@ -27,6 +27,7 @@ if(!$user->is_logged_in()){ header('Location: index.php'); }
     <link rel="stylesheet" href="style/materialize.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
     <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
