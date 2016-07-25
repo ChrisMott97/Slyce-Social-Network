ce59580be0126e719d6962f3e3fa0779346f58c9
Cigitec <alfa348@gmail.com>
2016-07-25 14:20:35 +0100

    chris

--- testtabs.php
+++ testtabs.php
@@ -20,19 +20,19 @@ if(!$user->is_logged_in()){ header('Location: index.php'); }
     <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
 <?php include('includes/navigation.php');?>
         
-        <div class="col">
-    <div class="row s12">
+        <script> $(document).ready(function(){
+    $('ul.tabs').tabs();
+  });</script>
+        
+<div class="row">
+    <div class="col s12">
       <ul class="tabs">
-        <li class="tab col s3"><a href="#test1">Test 1</a></li>
-        <li class="tab col s3"><a class="active" href="#test2">Test 2</a></li>
-        <li class="tab col s3 disabled"><a href="#test3">Disabled Tab</a></li>
-        <li class="tab col s3"><a href="#test4">Test 4</a></li>
+        <li class="tab col s3"><a href="#test1">Edit Bio</a></li>
+          <li class="tab col s3"><a href="#test2">Ed Bio</a></li>
       </ul>
     </div>
-    <div id="test1" class="row s12">Test 1</div>
-    <div id="test2" class="row s12">Test 2</div>
-    <div id="test3" class="row s12">Test 3</div>
-    <div id="test4" class="row s12"">Test 4</div>
+    <div id="test1" class="col s12">Edit Bio</div>
+    <div id="test2" class="col s12">Ed Bio</div>
   </div>
         
         
