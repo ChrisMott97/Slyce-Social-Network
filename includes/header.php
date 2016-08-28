6ac209b940cf534c42f4d5171983ad98978c75c8
Chris <christopheramott@sky.com>
2016-08-28 19:58:07 +0100

    Added style to cover page

--- includes/header.php
+++ includes/header.php
@@ -1,2 +1,3 @@
 <link rel='stylesheet' type='text/css' href='public/css/base.css'>
+<link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
--- views/index.view.php
+++ views/index.view.php
@@ -7,12 +7,34 @@
 
 <body>
     <div class='container'>
-        <div class='row center'>
-            <div class='col s4 offset-s4'>
-                <h1>Slyce</h1>
-                <form>
-                    
-                </form>
+        <div class='row'>
+            <div class='col s4 offset-s4 main'>
+                <span class="center title"><h1>Slyce</h1></span>
+                <div class="card-panel">
+                    <div class="row">
+                        <form class="col s12">
+                            <div class="row center">
+                                <h4>Login</h4></div>
+                            <div class="row">
+                                <div class="input-field col s12">
+                                    <input id="email" type="email" class="validate">
+                                    <label for="email">Email</label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="input-field col s12">
+                                    <input id="password" type="password" class="validate">
+                                    <label for="password">Password</label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <button class="btn waves-effect waves-light" type="submit" name="action">Login
+                                    <i class="material-icons right">send</i>
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
