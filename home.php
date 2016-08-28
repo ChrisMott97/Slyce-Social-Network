9dd1a096e49599c8f5f5d3677998c65e0c4cece3
Chris Mott <christopheramott@sky.com>
2016-08-28 21:42:23 +0100

    Enabled php debugging

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <?php
-
+ini_set('display_errors', 1);
 require 'includes/setup.php';
 
 if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == false){
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+ini_set('display_errors', 1);
 require 'includes/setup.php';
 
 if(isset($_POST['email'])){
